2155e3c85c73c6d4f2a3f2ec17d9f11460d7de6e
Mathieu <mathieu.boez@gmail.com>
2020-09-08 12:50:06 +0200

    Added front-end project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,16 +1,24 @@
 import blenderUnityImg from '../assets/blenderUnity.png';
 import constructionImg from '../assets/construction.png';
+import reactImg from '../assets/react_logo.png';
 
 const PROJECTS = [
     {
         id: 1,
         title: 'Video Game',
-        description: 'Video game project with Unity and Blender (WIP)',
+        description: 'Video game project with Unity and Blender + Udemy classes',
         link: 'https://github.com/darzang/GameName',
         image: blenderUnityImg,
     },
     {
         id: 2,
+        title: 'Front-end Development',
+        description: 'Learning front-end development with React. This portfolio is my first React project.',
+        link: 'https://mathieu-boez.netlify.app/',
+        image: reactImg,
+    },
+    {
+        id: 3,
         title: 'Others',
         description: 'Coming soon...',
         image: constructionImg,
